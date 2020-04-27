6f38be793cacb56a349a78072652611159151966
hiiamtrong <54925853+hiiamtrong@users.noreply.github.com>
2020-04-27 20:42:36 +0700

    commit

--- src/components/music/music.js
+++ src/components/music/music.js
@@ -6,10 +6,29 @@ let songlist= [
     singer:'buitruonglinh'
 },
 {
+    title:"5.AM",
+    src: './music/5AM - Ca Hoi Hoang.mp3',
+    banner:'./picture/5am.jpg',
+    singer:'Cá hồi hoang'
+},
+{
+    title:"Hương hồng hoa",
+    src: './music/HuongHongHoa.mp3',
+    banner:'./picture/hhh.jpg',
+    singer:'Tofu ft PC'
+},
+
+{
     title:"Hà nội xịn",
     src: './music/Ha-Noi-Xin-LK.mp3',
     banner:'./picture/ha-noi-xin.jpg',
     singer:'LK'
+},
+{
+    title:"Mây lang thang",
+    src: './music/May Lang Thang - Tung TeA_ PC_ New_oulZ.mp3',
+    banner:'./picture/may-lang-thang.jpg',
+    singer:'TeA ft PC'
 },
 {
     title:"Một mình ta",
@@ -46,6 +65,30 @@ let songlist= [
     src: './music/Tang-Thuong-102-Ca-Hoi-Hoang.mp3',
     banner:'./picture/tang-thuong-102.jpg',
     singer:'Cá hồi hoang '
+},
+{
+    title:"Không tán tỉnh em đâu",
+    src: './music/Khong-Tan-Tinh-Em-Dau-Tung-TeA-PC.mp3',
+    banner:'./picture/khong-tted.jpg',
+    singer:'TeA ft PC '
 }
 ]
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+   
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+   
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+   
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+   return array
+  }
+songlist = shuffle(songlist)
 export default songlist
